6d11ea5e9f8d9eac1ef6acf9194ec3ae63adbe39
Renzo <152234907+rlopezlo3434@users.noreply.github.com>
2024-12-09 18:58:01 -0500

    .

--- config/database.js
+++ config/database.js
@@ -1,24 +1,24 @@
 const { Sequelize } = require('sequelize');
 const config = require('./config.js')[process.env.NODE_ENV || 'development'];
 
-const sequelize = new Sequelize(config.database, config.username, config.password, {
-  host: config.host,
-  dialect: config.dialect,
-});
+// const sequelize = new Sequelize(config.database, config.username, config.password, {
+//   host: config.host,
+//   dialect: config.dialect,
+// });
 
-module.exports = sequelize;
+// module.exports = sequelize;
 
 
 // const { Sequelize } = require('sequelize');
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL || 'mysql://root:jmzbQQIKPKvUTKyWcraZlghouAPFFSEi@junction.proxy.rlwy.net:46943/railway', {
-//   dialect: 'mysql',
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false,
-//     },
-//   },
-// });
+const sequelize = new Sequelize(process.env.DATABASE_URL || 'mysql://root:jmzbQQIKPKvUTKyWcraZlghouAPFFSEi@junction.proxy.rlwy.net:46943/railway', {
+  dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
-// module.exports = sequelize;
+module.exports = sequelize;
